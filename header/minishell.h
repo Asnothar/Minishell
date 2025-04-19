fdab5b57cbeebbd5ad4e1740c825e5ec5f34cc1b
Sebastien Jupille <sjupille@42lehavre.fr>
2025-04-19 14:36:21 +0200

    modif orga minishell

--- header/minishell.h
+++ header/minishell.h
@@ -10,34 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-# include "../sources/ultimate_libft/header/ultimate_libft.h"
-
+# include <bs_tree.h>
+# include <as_tree.h>
+# include <typedefs.h>
+# include <curses.h>
+# include <dirent.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <libft.h>
+# include <list.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
 # include <stdio.h>
-# include <unistd.h>
 # include <stdlib.h>
-# include <sys/types.h>
+# include <string.h>
+# include <sys/ioctl.h>
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <sys/wait.h>
-# include <fcntl.h>
+# include <termios.h>
+# include <unistd.h>
 
-// execve.c
-int		search_path(char **env);
-char	*join_path(char *dir, char *cmd);
-int		check_exec(char *argv, char **cmd, char **env, char *path_cmd);
-void	exec_path(char *argv, char **env);
 
-// pipex.c
-void	child_1(char *file, char *cmd, int pipefd[2], char **env);
-void	child_2(char *file, char *cmd, int pipefd[2], char **env);
-int		main(int argc, char **argv, char **env);
-
-// utils.c
-void	free_tab(char **str);
-void	free_all(char **str1, char **str2);
-void	exit_free(char **path, char **cmd, int status, int stat);
 
 #endif
