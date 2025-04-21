a15905d7f69633586dec090ccc0e0ea6b8e3d3d4
Sebastien Jupille <sjupille@42lehavre.fr>
2025-04-21 15:43:51 +0200

    modif header1

--- header/minishell.h
+++ header/minishell.h
@@ -1,61 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-<<<<<<< HEAD
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sjupille <sjupille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/17 17:42:19 by abeaufil          #+#    #+#             */
-/*   Updated: 2025/04/21 11:51:38 by sjupille         ###   ########.fr       */
-=======
-/*   pipex.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: abeaufil <abeaufil@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/17 17:42:19 by abeaufil          #+#    #+#             */
-/*   Updated: 2025/04/21 11:39:14 by abeaufil         ###   ########.fr       */
->>>>>>> master
+/*   Created: 2025/04/21 15:42:03 by sjupille          #+#    #+#             */
+/*   Updated: 2025/04/21 15:43:25 by sjupille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-<<<<<<< HEAD
-# include <curses.h>
-# include <dirent.h>
-# include <errno.h>
-# include <fcntl.h>
-# include <libft.h>
-# include <list.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/ioctl.h>
-# include <sys/stat.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <termios.h>
-# include <unistd.h>
 
 
-=======
-# include "../sources/ultimate_libft/header/ultimate_libft.h"
 
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <string.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-#include <termcap.h>
->>>>>>> master
-
-#endif
