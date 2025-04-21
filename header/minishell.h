37a69a4eeeeda5e69b3d204d40e26ce376bc9533
Arthur Beaufils <abeaufil@c2r4p1.42lehavre.fr>
2025-04-21 15:22:48 +0200

    test aasdasdasd

---
+++ header/minishell.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abeaufil <abeaufil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/17 17:42:19 by abeaufil          #+#    #+#             */
+/*   Updated: 2025/04/21 11:39:14 by abeaufil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../sources/ultimate_libft/header/ultimate_libft.h"
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <termcap.h>
+
+#endif
