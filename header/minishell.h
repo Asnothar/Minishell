24d51c7d7cfe529c1bdc41d30c79de654ca16b01
Arthur Beaufils <abeaufil@c1r6p5.42lehavre.fr>
2025-04-30 11:25:01 +0200

    master

--- header/minishell.h
+++ header/minishell.h
@@ -6,7 +6,7 @@
 /*   By: abeaufil <abeaufil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 17:42:19 by abeaufil          #+#    #+#             */
-/*   Updated: 2025/04/29 16:17:05 by abeaufil         ###   ########.fr       */
+/*   Updated: 2025/04/30 11:23:18 by abeaufil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,32 @@
 # include <termios.h>
 # include <sys/ioctl.h>
 # include <termcap.h>
+
+typedef struct s_shell
+{
+	char			**envp;	// Copie de l'environnement
+	char			*input;	// Ligne de commande entrée par l'utilisateur
+	struct s_cmd	*cmds;	// Liste chaînée de commandes parsées
+	int				last_exit_status;	// Code de retour de la dernière commande exécutée
+}	t_shell;
+
+typedef struct s_cmd
+{
+	char			**args;	// Arguments de la commande (argv)
+	char			*infile;	// Fichier en entrée (<)
+	char			*outfile;	// Fichier en sortie (>)
+	char			*appendfile;	// Fichier de sortie en mode append (>>)
+	char			*heredoc_delim;	// Délimiteur pour le here_doc (<<)
+	int				pipe_fd[2];	// Pipe vers la commande suivante (si besoin)
+	struct s_cmd	*next;	// Commande suivante (pour pipe ou multiples commandes)
+}	t_cmd;
+
+typedef struct s_env
+{
+	char			*key;	// Clé de l'environnement
+	char			*value;	// Valeur de l'environnement
+	struct s_env	*next;	// Prochain élément de la liste chaînée
+}	t_env;
 
 //	parsing
 //		parsing1.c
