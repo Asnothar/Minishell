ab03189ecc8345f822640c0049c9b853adbfeed4
Sebastien Jupille <sjupille76@gmail.com>
2025-05-04 17:39:50 +0200

    Update parsing_2.c: error message

--- sources/parsing/parsing_2.c
+++ sources/parsing/parsing_2.c
@@ -11,3 +11,11 @@
 /* ************************************************************************** */
 
 #include "../../header/minishell.h"
+
+void	err_message(char *message1, char *message2, char *message3)
+{
+	write(2, message1, ft_strlen(message1));
+	write(2, message2, ft_strlen(message2));
+	write(2, message3, ft_strlen(message3));
+}
+
