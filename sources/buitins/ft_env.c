199ae009bdd4ee10ea033121cae0fee116392efa
Sebastien Jupille <sjupille76@gmail.com>
2025-05-04 17:36:28 +0200

    Update ft_env.c

--- sources/buitins/ft_env.c
+++ sources/buitins/ft_env.c
@@ -12,3 +12,23 @@
 
 #include "../../header/minishell.h"
 
+void	ft_env(t_shell *minishell)
+{
+	int	i;
+
+	if (minishell->cmds && minishell->cmds->args[1])
+	{
+		err_message("minishell: ", minishell->cmds->args[1],
+			": No such file or directory\n");
+		minishell->last_exit_status = 1;
+		return ;
+	}
+	i = 0;
+	while (minishell->envp[i])
+	{
+		ft_putstr_fd(minishell->envp[i], 1);
+		write(1, "\n", 1);
+		i++;
+	}
+	minishell->last_exit_status = 0;
+}
