89d82cb75f7fa547ffd12a38065aeac7ed485603
Sebastien Jupille <sjupille76@gmail.com>
2025-05-07 20:00:19 +0200

    Creation unset.c

---
+++ sources/builtins/unset.c
@@ -0,0 +1,51 @@
+
+
+
+void	unset1(t_shell *minishell, t_env *tmp, t_env *prev, int i)
+{
+	while (tmp)
+	{		
+		if (ft_strcmp(tmp->key, minishell->cmds->args[i]) == 0)
+		{
+			if (!prev)
+			{
+				tmp = tmp->next;
+				free(minishell->envp->key);
+				free(minishell->envp->value);
+				free(minishell->envp);
+				minishell->envp = tmp;
+				return ;
+			}
+			prev->next = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			break ;
+		}
+		prev = tmp;
+		tmp = tmp -> next;
+	}
+}
+
+void	unset(t_shell *minishell, char **cmd)
+{
+	t_env	*prev;
+	t_env	*tmp;
+	int		i;
+
+	i = 0;
+	while (cmd[i])
+	{
+		if (!valid_export_arg(cmd[i]))
+		{
+			err_message("minishell: unset: `", cmd[i],
+				"': not a valid identifier\n");
+			minishell->last_exit_status = 1;
+			return ;				
+		}
+		prev = NULL;
+		tmp = minishell->envp;
+		unset1(minishell, tmp, prev, i);
+		i++;
+	}
+}
