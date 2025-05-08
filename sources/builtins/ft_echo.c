cf81a720c04c6e76a84137ced3bc52eee407474f
Arthur Beaufils <abeaufil@c2r8p1.42lehavre.fr>
2025-05-08 15:40:36 +0200

    Removal of Headers

--- sources/builtins/ft_echo.c
+++ sources/builtins/ft_echo.c
@@ -1,50 +1,41 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*   ft_echo.c                                          :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: sjupille <sjupille@student.42.fr>          +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2025/05/05 11:39:51 by sjupille          #+#    #+#             */
-// /*   Updated: 2025/05/05 14:33:39 by sjupille         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
 
-// #include "../../header/minishell.h"
 
-// static int	check_newline(char *str)
-// {
-// 	int	i;
 
-// 	if (!str || str[0] != '-' || str[1] != 'n')
-// 		return (0);
-// 	i = 2;
-// 	while (str[i] == 'n')
-// 		i++;
-// 	return (str[i] == '\0');
-// }
+#include "../../header/minishell.h"
 
-// void	ft_echo(t_shell *minishell)
-// {
-// 	t_cmd	*cmd = minishell->cmds;
-// 	int		i = 1;
-// 	int		no_newline = 0;
-// 	char	**args = cmd->args;
+static int	check_newline(char *str)
+{
+	int	i;
 
-// 	while (args[i] && check_newline(args[i]))
-// 	{
-// 		no_newline = 1;
-// 		i++;
-// 	}
-// 	while (args[i])
-// 	{
-// 		ft_putstr_fd(args[i], 1);
-// 		if (args[i + 1])
-// 			write(1, " ", 1);
-// 		i++;
-// 	}
-// 	if (!no_newline)
-// 		write(1, "\n", 1);
-// 	minishell->last_exit_status = 0;
-// }
+	if (!str || str[0] != '-' || str[1] != 'n')
+		return (0);
+	i = 2;
+	while (str[i] == 'n')
+		i++;
+	return (str[i] == '\0');
+}
+
+void	ft_echo(t_shell *minishell)
+{
+	t_cmd	*cmd = minishell->cmds;
+	int		i = 1;
+	int		no_newline = 0;
+	char	**args = cmd->args;
+
+	while (args[i] && check_newline(args[i]))
+	{
+		no_newline = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		if (args[i + 1])
+			write(1, " ", 1);
+		i++;
+	}
+	if (!no_newline)
+		write(1, "\n", 1);
+	minishell->last_exit_status = 0;
+}
 
--- sources/builtins/ft_pwd.c
+++ sources/builtins/ft_pwd.c
@@ -1,59 +1,50 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*   ft_pwd.c                                           :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: sjupille <sjupille@student.42.fr>          +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2025/05/01 17:47:55 by sjupille          #+#    #+#             */
-// /*   Updated: 2025/05/05 14:28:04 by sjupille         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
 
-// #include "../../header/minishell.h"
 
-// void	cd_oldpwd(t_shell *minishell)
-// {
-// 	t_env	*env;
-// 	char	path[PATH_MAX];
-// 	char	path2[PATH_MAX];
-// 	char	*oldpwd;
 
-// 	env = minishell->envp;
-// 	while (env)
-// 	{
-// 		if (ft_strcmp(env->key, "OLDPWD") == 0)
-// 		{
-// 			oldpwd = env->value;
-// 			if (chdir(oldpwd) == -1)
-// 			{
-// 				perror("cd");
-// 				minishell->last_exit_status = 1;
-// 				return;
-// 			}
-// 			printf("%s\n", oldpwd);
-// 			ft_pwd(minishell, getcwd(path, PATH_MAX), getcwd(path2, PATH_MAX));
-// 			ft_update_envp(minishell->envp, "OLDPWD", path);
-// 			ft_update_envp(minishell->envp, "PWD", path2);
-// 			minishell->last_exit_status = 0;
-// 			return;
-// 		}
-// 		env = env->next;
-// 	}
-// 	ft_putstr_fd("cd: OLDPWD not set\n", 2);
-// 	minishell->last_exit_status = 1;
-// }
+#include "../../header/minishell.h"
 
-// int	pwd(void)
-// {
-// 	char	path[PATH_MAX];
+void	cd_oldpwd(t_shell *minishell)
+{
+	t_env	*env;
+	char	path[PATH_MAX];
+	char	path2[PATH_MAX];
+	char	*oldpwd;
 
-// 	if (!getcwd(path, PATH_MAX))
-// 	{
-// 		ft_putstr_fd("getcwd ERROR!\n", 2);
-// 		return (1);
-// 	}
-// 	ft_putstr_fd(path, 1);
-// 	write(1, "\n", 1);
-// 	return (0);
-// }
+	env = minishell->envp;
+	while (env)
+	{
+		if (ft_strcmp(env->key, "OLDPWD") == 0)
+		{
+			oldpwd = env->value;
+			if (chdir(oldpwd) == -1)
+			{
+				perror("cd");
+				minishell->last_exit_status = 1;
+				return;
+			}
+			printf("%s\n", oldpwd);
+			ft_pwd(minishell, getcwd(path, PATH_MAX), getcwd(path2, PATH_MAX));
+			ft_update_envp(minishell->envp, "OLDPWD", path);
+			ft_update_envp(minishell->envp, "PWD", path2);
+			minishell->last_exit_status = 0;
+			return;
+		}
+		env = env->next;
+	}
+	ft_putstr_fd("cd: OLDPWD not set\n", 2);
+	minishell->last_exit_status = 1;
+}
+
+int	pwd(void)
+{
+	char	path[PATH_MAX];
+
+	if (!getcwd(path, PATH_MAX))
+	{
+		ft_putstr_fd("getcwd ERROR!\n", 2);
+		return (1);
+	}
+	ft_putstr_fd(path, 1);
+	write(1, "\n", 1);
+	return (0);
+}
